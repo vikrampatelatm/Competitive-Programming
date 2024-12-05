f7a88a4f45e2004be8dc52779e4b3f1003c748e7
vikram patel <107579052+vikrampatelatm@users.noreply.github.com>
2024-12-05 19:30:53 +0530

    Day-6

---
+++ GamewithInteger.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1899/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        if((n+1)%3==0 || (n-1)%3==0){
+            cout<<"First"<<endl;
+        }else{
+            cout<<"Second"<<endl;
+        }
+        
+    }
+    return 0;
+}
